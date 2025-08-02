63dd3d3463ee8b26eb262f0ba98de6bad31a6797
s566466Div <s566466@nwmissouri.edu>
2025-08-02 14:24:11 -0500

    added java project

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java CI/CD!");
+    }
+
+    public static String greet() {
+        return "Hello Jenkins";
+    }
+}
